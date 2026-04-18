2dbe267a87cf407829a804c1f4ea6a9c8290d9e3
willuhmruj-stack <willuhmruj@gmail.com>
2026-04-18 19:57:21 +0700

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUotTGRXmMNGRmVqwAZQaWPuoJLSVQzTE",
+  authDomain: "godruj-cf845.firebaseapp.com",
+  projectId: "godruj-cf845",
+  storageBucket: "godruj-cf845.firebasestorage.app",
+  messagingSenderId: "842695726741",
+  appId: "1:842695726741:web:11c1de5cc7458bf4cfad9c"
+};
+
+firebase.initializeApp(firebaseConfig);
